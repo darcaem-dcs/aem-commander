bff7dd76206755a99ca14d2ab1d56a21de6b4c52
darcaem-dcs <darcaem.dcs@gmail.com>
2026-06-27 19:31:26 +0200

    Updated threat values and logic

--- main.js
+++ main.js
@@ -244,7 +244,7 @@ function routeDCSMessage(msg) {
                     const isStatic = staticThreats.includes(item.threat);
                     
                     if (isStatic) {
-                        if (distKm < 40) multiplier = 0.2; // SAM/Artillería muy pegado a la frontera
+                        if (distKm < 20) multiplier = 0.5; // SAM/Artillería muy pegado a la frontera
                         else multiplier = 0;               // SAM defensivo lejano (no amenaza nuestra postura)
                     } else {
                         // Unidades móviles (CAP, STRIKE, HELO, ARMOR...)
